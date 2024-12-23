6f6f20e682b222f71a333a6e9dcfefee9dbf6e6a
Rémy Mondi <remy@mondi.dev>
2024-12-23 19:47:51 +0100

    [Add] Header & logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import Logo from "./Logo";
+
 const Header = () => {
   
   return (
-    <header className="h-10 bg-white">
-      <div className="flex justify-between items-center h-full px-5 sm:px-8 xl:px-0 xl:m-auto xl:max-w-5xl 2xl:max-w-7xl">
-        Header
+    <header className="h-12 bg-white">
+      <div className="flex justify-center items-center h-full px-5 sm:px-8 xl:px-0 xl:m-auto xl:max-w-5xl 2xl:max-w-7xl">
+        <Logo />
       </div>
     </header>
   )
